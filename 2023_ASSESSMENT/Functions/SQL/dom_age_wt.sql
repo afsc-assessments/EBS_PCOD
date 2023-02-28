1c87f7b70b48635e8769fe2595cea2c06a51e311
Steve Barbeaux <steve.barbeaux@noaa.gov>
2023-02-27 16:14:34 -0800

    Update dom_age_wt.sql

--- 2023_ASSESSMENT/Functions/SQL/dom_age_wt.sql
+++ 2023_ASSESSMENT/Functions/SQL/dom_age_wt.sql
@@ -1,26 +1,26 @@
 SELECT
-        obsint.debriefed_age.species,
-        obsint.debriefed_age.year,
-        TO_CHAR(obsint.debriefed_age.haul_offload_date, 'mm') AS month,
-        obsint.debriefed_age.haul_offload_date,
-        obsint.debriefed_age.cruise,
-        obsint.debriefed_age.vessel,
-        obsint.debriefed_age.gear,
-        obsint.debriefed_age.sex,
-        obsint.debriefed_age.length,
-        obsint.debriefed_age.weight
+        norpac.debriefed_age.species,
+        norpac.debriefed_age.year,
+        TO_CHAR(norpac.debriefed_age.haul_offload_date, 'mm') AS month,
+        norpac.debriefed_age.haul_offload_date,
+        norpac.debriefed_age.cruise,
+        norpac.debriefed_age.vessel,
+        norpac.debriefed_age.gear,
+        norpac.debriefed_age.sex,
+        norpac.debriefed_age.length,
+        norpac.debriefed_age.weight
     FROM
-        obsint.debriefed_age
+        norpac.debriefed_age
     WHERE
-        obsint.debriefed_age.species
+        norpac.debriefed_age.species
         -- insert species
-        AND obsint.debriefed_age.length IS NOT NULL
-        AND obsint.debriefed_age.weight IS NOT NULL
-        AND obsint.debriefed_age.nmfs_area 
+        AND norpac.debriefed_age.length IS NOT NULL
+        AND norpac.debriefed_age.weight IS NOT NULL
+        AND norpac.debriefed_age.nmfs_area 
         -- insert location
     ORDER BY
-        obsint.debriefed_age.year,
-        obsint.debriefed_age.cruise,
-        obsint.debriefed_age.vessel,
-        obsint.debriefed_age.length,
-        obsint.debriefed_age.weight
+        norpac.debriefed_age.year,
+        norpac.debriefed_age.cruise,
+        norpac.debriefed_age.vessel,
+        norpac.debriefed_age.length,
+        norpac.debriefed_age.weight
