74df3764dee784e11085d8a788adce8ca5d09655
Steve Barbeaux <steve.barbeaux@noaa.gov>
2023-02-27 16:44:53 -0800

    Update EBS_length_comp.sql

--- 2023_ASSESSMENT/Functions/SQL/EBS_length_comp.sql
+++ 2023_ASSESSMENT/Functions/SQL/EBS_length_comp.sql
@@ -1,13 +1,13 @@
 SELECT
-    afsc.race_sizecmp_ebs_plusnw.year,
-    afsc.race_sizecmp_ebs_plusnw.length /10 AS LENGTH,
-    afsc.race_sizecmp_ebs_plusnw.total AS EBS_TOTAL
+    afsc.race_sizecmp_ebsshelf_plusnw.year,
+    afsc.race_sizecmp_ebsshelf_plusnw.length /10 AS LENGTH,
+    afsc.race_sizecmp_ebsshelf_plusnw.total AS EBS_TOTAL
 FROM
-    afsc.race_sizecmp_ebs_plusnw 
+    afsc.race_sizecmp_ebsshelf_plusnw 
 WHERE
-    afsc.race_sizecmp_ebs_plusnw.species_code
+    afsc.race_sizecmp_ebsshelf_plusnw.species_code
 -- insert species
-    AND afsc.race_sizecmp_ebs_plusnw.subarea = 999   
+    AND afsc.race_sizecmp_ebsshelf_plusnw.subarea = 999   
 ORDER BY
-    afsc.race_sizecmp_ebs_plusnw.year,
-    afsc.race_sizecmp_ebs_plusnw.length 			
+    afsc.race_sizecmp_ebsshelf_plusnw.year,
+    afsc.race_sizecmp_ebsshelf_plusnw.length 			
